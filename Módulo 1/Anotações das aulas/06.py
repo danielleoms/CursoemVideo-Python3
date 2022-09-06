864766ac745e57e0c8bc55e266cae7096610560d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-06 13:33:19 -0300

    Delete Módulo 1 directory

--- Módulo 1/Anotações das aulas/06.py
+++
@@ -1,4 +0,0 @@
-nota1= float(input("Informe a primeira nota:"))
-nota2= float(input("Informe a segunda nota:"))
-media = (nota1 + nota2)/2
-print('A sua nota é {}' .format(media))
--- Módulo 1/Anotações das aulas/07.py
+++
@@ -1,2 +0,0 @@
-n = float(input('Digite um valor:'))
-print(type(n))
