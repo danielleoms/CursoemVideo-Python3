b8bd7bf7c42b8213ea3508f9180a2c670e7b6e2a
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-06 13:40:55 -0300

    Delete Anotações das aulas directory

--- Anotações das aulas/05.py
+++
@@ -1,6 +0,0 @@
-
-n1 = int(input('Digite um número:'))
-n2 = int(input('Digite um número:'))
-s= n1 + n2
-#print('A soma vale entre', n1,'e', n2,'vale', s , ',parabéns!')
-print('A soma entre {} e {} vale {}, parabéns!' .format(n1, n2, s))
