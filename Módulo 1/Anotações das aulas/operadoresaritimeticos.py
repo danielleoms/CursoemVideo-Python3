2e4857a3918e008a23a36e021876505bf3777147
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-14 16:09:19 -0300

    Create operadoresaritimeticos.py

---
+++ Módulo 1/Anotações das aulas/operadoresaritimeticos.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+
+# + adição
+#5 + 2 = = 2
+
+# - subtração
+#5 - 2 = = 3
+
+# * multiplicação
+#5 * 2 = = 10
+
+# / divisão
+#5 / 2 = = 2.5
+
+# ** potencia
+#5 ** 2 = = 25
+
+# // divisão inteira
+#5 // 2 = = 25
+
+# % módulo: resto da divisão
+#5 %2 = = 1
