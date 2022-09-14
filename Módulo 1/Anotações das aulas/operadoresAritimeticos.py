73c205e5aa40287394289dc1e85b2cd15c8ca3b7
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-14 16:37:23 -0300

    Update operadoresAritimeticos.py

--- Módulo 1/Anotações das aulas/operadoresAritimeticos.py
+++ Módulo 1/Anotações das aulas/operadoresAritimeticos.py
@@ -1,22 +1,23 @@
 # Operadores aritméticos
 
 # + adição
-#5 + 2 = = 2
-
 # - subtração
-#5 - 2 = = 3
-
 # * multiplicação
-#5 * 2 = = 10
-
 # / divisão
-#5 / 2 = = 2.5
-
 # ** potencia
-#5 ** 2 = = 25
-
 # // divisão inteira
-#5 // 2 = = 25
-
 # % módulo: resto da divisão
-#5 %2 = = 1
+
+
+n1 = int(input('Um valor:'))
+n2 = int(input('Outro valor:'))
+s = n1 + n2
+m = n1 * 2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma vale {}, \n o produto é {} e a divisão é {:.3f}.' .format(s, m, d), end=' ')
+print('Divisão inteira {} e potência {}' .format(di,e))
+
+# \n quebra a linha e end=' ' junta linha dos prints
+
