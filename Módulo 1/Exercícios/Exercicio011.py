072a8516fbb8189617c041edd6925f471fa73c2a
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 12:13:15 -0300

    Update Exercicio011.py

--- Módulo 1/Exercícios/Exercicio011.py
+++ Módulo 1/Exercícios/Exercicio011.py
@@ -1,1 +1,10 @@
-oi
+#Exercício Python 11: Faça um programa que leia a largura e a altura de uma parede em metros,
+# calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+lar = float(input('Largura da parede:'))
+al = float(input('Altura da parede:'))
+area = lar * al
+tinta = area/2
+
+
+print('Sua parede tem a dimensão de {} X {} e sua área é de {}m². \nPara pintar essa parede, você precisará de {}l de tinta.' .format(lar,al,area, tinta))
