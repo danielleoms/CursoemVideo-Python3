d96c8cd6108c8eb9263c0ed8405a1bda50bdc060
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 13:11:02 -0300

    Create Exercicio013.py

---
+++ Módulo 1/Exercícios/Exercicio013.py
@@ -0,0 +1,9 @@
+#Exercício Python 13: Faça um algoritmo que leia o
+# salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input('Qual é o salário do funcionário? R$'))
+nv = salario + (salario * 15/100)
+
+print('O salário do funcionário era de R${:.2f}, com o reajuste de 15% de aumento agora é de R${:.2f}.' .format(salario,nv))
+
+
