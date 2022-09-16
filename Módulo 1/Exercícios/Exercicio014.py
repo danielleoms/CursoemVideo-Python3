63dfebbe07f9ee9f9a32f8f41de012c264068483
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-16 13:22:18 -0300

    Update Exercicio014.py

--- Módulo 1/Exercícios/Exercicio014.py
+++ Módulo 1/Exercícios/Exercicio014.py
@@ -4,6 +4,6 @@
 #A equação é °F = (°C x 1,8) + 32. Você também pode usar a fórmula °F = °C x 9 ÷ 5 + 32
 
 celcius = float(input('Informe a temperatura em C:'))
-fahrenheit = (( celcius * 9 ) / 5) + 32
+fahrenheit =  (celcius * 9 ) / 5 + 32
 
 print('A temperatura de {:.2f}ºC corresponde a {:.2f}ºF.' .format(celcius,fahrenheit))
