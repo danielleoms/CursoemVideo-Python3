63f8247a135a92e51439527aefd25ad59a69f84b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 00:48:46 -0300

    Delete Exercicio009.py

--- Módulo 1/Exercícios/Exercicio009.py
+++
@@ -1,8 +0,0 @@
-#Exercício Python 009: Faça um programa que leia
-#um número Inteiro qualquer e mostre na tela a sua tabuada.
-
-
-num = int(input('Digite um número inteiro para ver sua tabuada:'))
-for cont in range (0,11):
-    tab = cont * num
-    print('{} X {} = {}' .format(num,cont,tab))
