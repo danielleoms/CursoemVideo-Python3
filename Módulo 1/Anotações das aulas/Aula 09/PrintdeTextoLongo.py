fedc2b071553fa848a19a46f86073e2d3598089d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 02:29:19 -0300

    Create PrintdeTextoLongo.py

---
+++ Módulo 1/Anotações das aulas/Aula 09/PrintdeTextoLongo.py
@@ -0,0 +1,9 @@
+# Ao utilizar aspas triplas dentro do print, exibe um texto inteiro com várias linhas sem precisar repetir o "print" em cada uma delas.
+
+print('oi!')
+print("""Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Cras aliquam sapien non augue pharetra finibus. Integer ac justo quam.
+Pellentesque at eros vel tortor pharetra mollis sit amet convallis sem.
+Cras vel lobortis metus. Donec metus magna, fermentum eget dolor at, egestas dapibus sem.
+Etiam varius, enim ut tincidunt tristique, dui risus tristique quam, id iaculis ipsum sem nec elit.
+Nunc iaculis sit amet diam id lobortis. Nam egestas congue lectus vitae maximus.""")
