ad406c8246da77f5566e42a91609fe2d78fca121
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 00:45:16 -0300

    Dobro, triplo e raiz Quadrada

---
+++ Módulo 1/Exercícios/Exercicio006.py
@@ -0,0 +1,10 @@
+#EXERCÍCIO 006: Dobro, Triplo, Raiz Quadrada
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('Digite um número:'))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
+print('O dobro de {} vale {}' .format(n, d))
+print('O triplo de {} vale {}, e a raiz quadrada vale {:.3f}.' .format(n,t,r))
