d57cbd5158d08c27be1db2c85ac65c68385ab708
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 01:09:31 -0300

    Conversor de moedas

--- Módulo 1/Exercícios/Exercicio010.py
+++ Módulo 1/Exercícios/Exercicio010.py
@@ -1,5 +1,4 @@
-#Exercício Python 010: Crie um programa que leia quanto
-# dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+#Exercício Python 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
 cotdol= float(input('Valor da cotação do dólar hoje em relação ao real:'))
 di = float(input('Quantos reias (R$) você tem na carteira?'))
