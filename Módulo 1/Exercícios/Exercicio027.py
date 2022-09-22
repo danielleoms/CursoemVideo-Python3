db7daa453a37831dcd001e349a42a22c22545cbe
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 03:30:48 -0300

    Create Exercicio027.py

---
+++ Módulo 1/Exercícios/Exercicio027.py
@@ -0,0 +1,13 @@
+"""
+EXERCÍCIO 027: Primeiro e Último Nome de uma Pessoa
+Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro
+e o último nome separadamente.
+Ex: Danielle Oliveira Moreno de Souza
+Primeiro = Danielle
+Último = Souza
+"""
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}.'.format(nome[0]))
+print('Seu último nome é {}.'.format(nome[len(nome) - 1]))
