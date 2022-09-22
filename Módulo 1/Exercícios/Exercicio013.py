51017764430063169f5360d225509e2f43317ab3
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 00:52:55 -0300

    Reajuste salarial

--- Módulo 1/Exercícios/Exercicio013.py
+++ Módulo 1/Exercícios/Exercicio013.py
@@ -1,5 +1,5 @@
 #Exercício Python 13: Faça um algoritmo que leia o
-# salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+#salário de um funcionário e mostre seu novo salário, com 15% de aumento.
 
 salario = float(input('Qual é o salário do funcionário? R$'))
 nv = salario + (salario * 15/100)
