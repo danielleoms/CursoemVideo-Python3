b9d3725ea308aef190d8acacc30b65b54a9e8952
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-22 00:53:47 -0300

    Conversor de temperaturas

--- Módulo 1/Exercícios/Exercicio014.py
+++ Módulo 1/Exercícios/Exercicio014.py
@@ -1,5 +1,4 @@
-#Exercício Python 14: Escreva um programa que converta
-# uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+#Exercício Python 14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 
 #A equação é °F = (°C x 1,8) + 32. Você também pode usar a fórmula °F = °C x 9 ÷ 5 + 32
 
