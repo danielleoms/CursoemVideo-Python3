765ce2b1551c2d693f81f44e94adaa4934b936b0
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 13:05:59 -0300

    Par ou Ímpar?

---
+++ Módulo 1/Exercícios/Exercicio030.py
@@ -0,0 +1,7 @@
+#Exercício Python 30: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
+
+num = int(input('Informe um número:'))
+if num % 2 == 0:
+    print('{} é um número par.' .format(num))
+else:
+    print('{} é um número par.' .format(num))
