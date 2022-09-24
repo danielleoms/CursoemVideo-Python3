07cd3acb0816fa09377a4b6f34ce7bdfea3a570b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 16:06:37 -0300

    Delete condicaoComposta-Media.py

--- Módulo 1/Anotações das aulas/condicaoComposta-Media.py
+++
@@ -1,9 +0,0 @@
-n1 = float(input('Digite a primeira nota:'))
-n2 = float(input('Digite a segunda nota:'))
-m = (n1 + n2)/2
-print('A sua média foi {:.1f}' .format(m))
-
-if m >= 6:
-    print('Sua média foi boa! PARABÉNS')
-else:
-    print('Sua média foi ruim! ESTUDE MAIS!')
