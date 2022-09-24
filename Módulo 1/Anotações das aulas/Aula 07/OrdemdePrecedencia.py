aef39081962fe0e9786b6c808dc5d5b3519d5666
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 15:27:44 -0300

    Create OrdemdePrecedencia.py

---
+++ Módulo 1/Anotações das aulas/Aula 07/OrdemdePrecedencia.py
@@ -0,0 +1,7 @@
+
+#Ordem de Precedência (O que é levado em conta primeiro no código do Python):
+#1 () - Parênteses
+#2 ** - Potência/Exponenciação
+#3 *, /, // e % - Multiplicação, Divisão, Divisão Inteira e Resto da Divisão (O que aparecer primeiro no código, executa primeiro)
+#4 + e - - Adição e Subtração (O que aparecer primeiro no código, executa primeiro)
+
