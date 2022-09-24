d5b2b9126b2b2c43c448e311678f77b72b748198
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 15:37:20 -0300

    Create utilizaçãodoMaisVirgula.py

---
+++ Módulo 1/Anotações das aulas/Aula 04/utilizaçãodoMaisVirgula.py
@@ -0,0 +1,10 @@
+print(7 + 4)  # 7 + 4 = 11 #soma dos números
+
+print('7' + '4')  # '7' + '4' = '74' #junta os númeors 
+
+print('7', '4')  # '7' , '4' = '7 4' #separa eles
+
+""" NOTA: Uma mensagem e um número é possivel no mesmo print utilizando a vírgula, mas não o símbolo de "+".
+Usando a vírgula, adiciona-se um espaço entre os valores dentro do print."""
+
+print('Olá', 5)  # 'Olá' , 5 = 'Olá 5'
