9757bd664f325bc4a43655febd42220dd6908218
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 16:06:57 -0300

    Create estruturaCondicional.py

---
+++ Módulo 1/Anotações das aulas/Aula 10/estruturaCondicional.py
@@ -0,0 +1,17 @@
+#If para estrutura simples
+
+#If e else para estrutura composta
+
+#Todo comando que estiver com indentação
+
+#Todo comando que estiver alinhado a esquerda vai ser executado
+
+tempo=int(input('Quantos anos tem seu carro?'))
+
+if tempo <=3:
+    print('Carro novo')
+
+else:
+    print('Carro velho')
+    
+print('--FIM--')
