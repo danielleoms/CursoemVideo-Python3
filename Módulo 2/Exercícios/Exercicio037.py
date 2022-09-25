3ea056f2ca9c3ed6b61905b567e51ac342ecbb6d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 23:22:55 -0300

    Conversor de Bases Numéricas

--- Módulo 2/Exercícios/Exercicio037.py
+++ Módulo 2/Exercícios/Exercicio037.py
@@ -1,6 +1,6 @@
 
-#Exercício Python 37: Escreva um programa em Python que leia um número inteiro
-# qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
+#Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e 
+#peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
 
 """print('-=-' * 10)
 num = int(input('Informe um número inteiro:'))
