0dc0f92f42ba11bd31b7d68d18b2d02468e7cf0f
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 23:23:22 -0300

    Comparando números

--- Módulo 2/Exercícios/Exercicio038.py
+++ Módulo 2/Exercícios/Exercicio038.py
@@ -1,7 +1,7 @@
 #Exercício Python 038: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
-#– O primeiro valor é maior
-#– O segundo valor é maior
-#– Não existe valor maior, os dois são iguais
+# – O primeiro valor é maior
+# – O segundo valor é maior
+# – Não existe valor maior, os dois são iguais
 
 num1 = int(input('Primeiro número:'))
 num2 = int(input('Segundo número:'))
