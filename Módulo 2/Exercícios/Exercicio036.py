b6b5f601c19271a6e69743da6e498f9474b73fa1
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2022-09-24 23:22:09 -0300

    Aprovando Empréstimo

--- Módulo 2/Exercícios/Exercicio036.py
+++ Módulo 2/Exercícios/Exercicio036.py
@@ -1,6 +1,6 @@
 #Exercício Python 36: Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa.
 # Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar.
-# A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+#A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
 
 vcasa = float(input('Valor da casa: R$'))
 salario = float(input('Salário do comprador: R$'))
